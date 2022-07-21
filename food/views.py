ab06de1334a43dde7e498084c9eef6f93724f4ba
Ukn Subedi <yukensubedi@gmail.com>
2022-07-21 22:43:38 +0545

    First commit

---
+++ food/views.py
@@ -0,0 +1,24 @@
+from urllib.request import Request
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+from django.template import loader
+# Create your views here.
+def index (request):
+    item_list = Item.objects.all()
+    template = loader.get_template('food/index.html')
+    context = {
+            'item_list': item_list, 
+    }
+    return HttpResponse(template.render(context,request))
+
+def item (request):
+    return HttpResponse('This is an item view')
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item' : item,
+    }
+    return render(request, 'food/detail.html', context)
